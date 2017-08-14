d863b9ea02847e98b14bfff8ac28e633e16ae092
BITUIGA <bituiga@oocl.com>
2017-08-14 20:19:14 +0800

    Exercises and Assignments for Aug 8

---
+++ BITUIGA/08AUG2017/BITUIGA08AUG/src/com/oocl/bituiga/sw1/DivideByZeroo.java
@@ -0,0 +1,17 @@
+package com.oocl.bituiga.sw1;
+
+public class DivideByZeroo {
+
+	public DivideByZeroo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+//	public int divideNum() {
+//		
+//	}
+}
