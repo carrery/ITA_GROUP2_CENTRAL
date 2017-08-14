890e284ed71ccdfc09c55a76a9fbe8d6d9594441
BITUIGA <bituiga@oocl.com>
2017-08-14 20:21:36 +0800

    Exercises for Aug 7

---
+++ BITUIGA/07AUG2017/BITUIGA07AUG/src/com/oocl/bituiga/sw2/MainClas.java
@@ -0,0 +1,25 @@
+package com.oocl.bituiga.sw2;
+import com.oocl.bituiga.sw1.HolderClass;
+
+public class MainClas {
+
+	public MainClas() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HolderClass hello = null;
+		try {
+			System.out.println(hello.getMember(1));
+		}
+		catch(Exception e) {
+			System.out.println("Caught exception: " + e);
+		}
+		
+		
+		
+	}
+
+}
