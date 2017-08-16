1051fe722b786baa16ea0037a3defa5e27baa256
BITUIGA <bituiga@oocl.com>
2017-08-16 19:19:04 +0800

    Aug 16 Exercises

---
+++ BITUIGA/16AUG2017/BITUIGA16AUG/src/com/oocl/bituiga/sw2/Drawing.java
@@ -0,0 +1,13 @@
+package com.oocl.bituiga.sw2;
+
+
+public class Drawing {
+
+	public Drawing() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void draw(Shape ship) {
+		System.out.println("Drawing " + ship.type);
+	}
+}
---
+++ BITUIGA/16AUG2017/BITUIGA16AUG/src/com/oocl/bituiga/sw2/Triangle.java
@@ -0,0 +1,17 @@
+package com.oocl.bituiga.sw2;
+
+public class Triangle extends Shape{
+
+	public Triangle() {
+	}
+
+	
+//	
+//	public String getType() {
+//		return type;
+//	}
+//
+//	public void setType(String type) {
+//		this.type = type;
+//	}
+}
