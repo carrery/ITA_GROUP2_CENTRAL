0da816b1b9e002abd0b06fbad44f817b714a47b9
yapojo <yapojo@ZHA-ITA003-W7.corp.oocl.com>
2017-08-23 09:23:45 +0800

    Day 17: Assignment

--- YAPOJO/22AUG2017/YAPOJO22AUG/src/com/oocl/ita/yapojo/sw/MainCalculator.java
+++ YAPOJO/22AUG2017/YAPOJO22AUG/src/com/oocl/ita/yapojo/sw/MainCalculator.java
@@ -1,4 +1,4 @@
-package com.oocl.ita.yapo.sw;
+package com.oocl.ita.yapojo.sw;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
