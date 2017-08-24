278326a99d3825ac7fe85d214282a8ce8a17a482
Jag Gervacio <GERVAJO@ZHA-ITA129-W7.corp.oocl.com>
2017-08-24 14:03:14 +0800

    Exercises for Aug 09 and Aug 22

---
+++ GERVAJO/22AUG2017/GERVAJO22AUG/src/com/oocl/ita/yapo/day17/webservice/GetOperations.java
@@ -0,0 +1,32 @@
+
+package com.oocl.ita.yapo.day17.webservice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for getOperations complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="getOperations">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getOperations")
+public class GetOperations {
+
+
+}
---
+++ GERVAJO/22AUG2017/GERVAJO22AUG/src/com/oocl/ita/yapo/day17/webservice/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.day17.yapo.ita.oocl.com/")
+package com.oocl.ita.yapo.day17.webservice;
