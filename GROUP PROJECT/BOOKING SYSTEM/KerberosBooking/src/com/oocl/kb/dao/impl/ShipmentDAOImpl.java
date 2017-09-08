db530c92dc333f5c2a65f9b31cc2d011b2ab6e47
Jag Gervacio <GERVAJO@ZHA-ITA129-W7.corp.oocl.com>
2017-09-08 22:54:32 +0800

    Shipment Model and DAO

---
+++ GROUP PROJECT/BOOKING SYSTEM/KerberosBooking/src/com/oocl/kb/dao/impl/ShipmentDAOImpl.java
@@ -0,0 +1,41 @@
+package com.oocl.kb.dao.impl;
+
+import java.util.ArrayList;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.oocl.kb.dao.inf.ShipmentDAO;
+import com.oocl.kb.model.ShpShipment;
+
+public class ShipmentDAOImpl implements ShipmentDAO {
+
+	@Autowired
+	SessionFactory sessionFactory;
+	Session session;
+	Transaction tx;
+
+	@Override
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@Override
+	public void createBooking() {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.openSession();
+		Transaction tx = null;
+
+		tx = session.beginTransaction();
+		ShpShipment newShp = new ShpShipment("MNL", null, null, null, null, null, 0, 0, 0, null);
+		session.save(newShp);
+		tx.commit();
+
+		session.close();
+		System.out.println("Booking Created");
+
+	}
+}
---
+++ GROUP PROJECT/BOOKING SYSTEM/KerberosBooking/src/com/oocl/kb/model/ShpShipment.java
@@ -0,0 +1,202 @@
+package com.oocl.kb.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "SHP_SHIPMENT")
+public class ShpShipment {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="shpNumSeq")
+	@SequenceGenerator(name="shpNumSeq", sequenceName="SHP_NUM_SEQ")
+	@Column(name="SHIPMENT_NUM")
+	private Long shipmentNum;
+	
+	@Column(name="FROM_CITY")
+	private String fromCity;
+	
+	@Column(name="TO_CITY")
+	private String toCity;
+	
+	@Column(name="FROM_DATE")
+	private Timestamp fromDate;
+	
+	@Column(name="DATE_TO")
+	private Timestamp toDate;
+	
+	@Column(name="SHIPPER")
+	private String shipper;
+	
+	@Column(name="CONSIGNEE")
+	private String consignee;
+	
+	@Column(name="APPROVE_DOC")
+	private int approveDoc;
+	
+	@Column(name="VALID_WT")
+	private int validWt;
+	
+	@Column(name="GOOD_CUSTOMER")
+	private int goodCustomer;
+	
+	@Column(name="SHIPMENT_STATUS")
+	private String shipmentStatus;
+	
+	@Column(name="CREATE_BY")
+	private String createBy;
+	
+	@Column(name="CREATE_DATE")
+	private Timestamp createDate;
+	
+	@Column(name="UPDATE_BY")
+	private String updateby;
+	
+	@Column(name="UPDATE_DATE")
+	private Timestamp updateDate;
+	
+	
+
+	public ShpShipment(String fromCity, String toCity, Timestamp fromDate, Timestamp toDate, String shipper,
+			String consignee, int approveDoc, int validWt, int goodCustomer, String shipmentStatus) {
+		super();
+		this.fromCity = fromCity;
+		this.toCity = toCity;
+		this.fromDate = fromDate;
+		this.toDate = toDate;
+		this.shipper = shipper;
+		this.consignee = consignee;
+		this.approveDoc = approveDoc;
+		this.validWt = validWt;
+		this.goodCustomer = goodCustomer;
+		this.shipmentStatus = shipmentStatus;
+	}
+
+	public Long getShipmentNum() {
+		return shipmentNum;
+	}
+
+	public void setShipmentNum(Long shipmentNum) {
+		this.shipmentNum = shipmentNum;
+	}
+
+	public String getFromCity() {
+		return fromCity;
+	}
+
+	public void setFromCity(String fromCity) {
+		this.fromCity = fromCity;
+	}
+
+	public String getToCity() {
+		return toCity;
+	}
+
+	public void setToCity(String toCity) {
+		this.toCity = toCity;
+	}
+
+	public Timestamp getFromDate() {
+		return fromDate;
+	}
+
+	public void setFromDate(Timestamp fromDate) {
+		this.fromDate = fromDate;
+	}
+
+	public Timestamp getToDate() {
+		return toDate;
+	}
+
+	public void setToDate(Timestamp toDate) {
+		this.toDate = toDate;
+	}
+
+	public String getShipper() {
+		return shipper;
+	}
+
+	public void setShipper(String shipper) {
+		this.shipper = shipper;
+	}
+
+	public String getConsignee() {
+		return consignee;
+	}
+
+	public void setConsignee(String consignee) {
+		this.consignee = consignee;
+	}
+
+	public int getApproveDoc() {
+		return approveDoc;
+	}
+
+	public void setApproveDoc(int approveDoc) {
+		this.approveDoc = approveDoc;
+	}
+
+	public int getValidWt() {
+		return validWt;
+	}
+
+	public void setValidWt(int validWt) {
+		this.validWt = validWt;
+	}
+
+	public int getGoodCustomer() {
+		return goodCustomer;
+	}
+
+	public void setGoodCustomer(int goodCustomer) {
+		this.goodCustomer = goodCustomer;
+	}
+
+	public String getShipmentStatus() {
+		return shipmentStatus;
+	}
+
+	public void setShipmentStatus(String shipmentStatus) {
+		this.shipmentStatus = shipmentStatus;
+	}
+
+	public String getCreateBy() {
+		return createBy;
+	}
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+
+	public Timestamp getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Timestamp createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getUpdateby() {
+		return updateby;
+	}
+
+	public void setUpdateby(String updateby) {
+		this.updateby = updateby;
+	}
+
+	public Timestamp getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Timestamp updateDate) {
+		this.updateDate = updateDate;
+	}
+
+}
