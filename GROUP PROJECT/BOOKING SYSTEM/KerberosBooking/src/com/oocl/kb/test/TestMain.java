d5a183fc4336a73a064afe15e2532d3ac4f7a1ae
Gayle Bituin <bituiga@oocl.com>
2017-09-08 22:55:33 +0800

    Refactor TestMain , added servlet-context.xml and update build config for testing only

--- GROUP PROJECT/BOOKING SYSTEM/KerberosBooking/src/com/oocl/kb/test/TestMain.java
+++ GROUP PROJECT/BOOKING SYSTEM/KerberosBooking/src/com/oocl/kb/test/TestMain.java
@@ -1,5 +1,7 @@
 package com.oocl.kb.test;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,16 +10,40 @@ import com.oocl.kb.model.Role;
 import com.oocl.kb.model.User;
 
 public class TestMain {
-
+	
+	static ApplicationContext ctx = new ClassPathXmlApplicationContext("servlet-context.xml");
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("servlet-context.xml");
+		// TODO this is to test java side logic without UI
+		// added local copy of servlet-context.xml and ojdb6 jar in build 
+		
+		// create separate test units here and comment unused tests
+		
+//		test_validateUser();
+//		test_getUserRole();
+		test_getAllUsersByUsername();
+		
+	}
+	
+	static void test_validateUser(){		
+		UserDAOImpl userDAO = (UserDAOImpl) ctx.getBean("userDAO");
+		User user = userDAO.getUserDetails("jag123", "admin");
+		System.out.println(userDAO.validateUser("jag123", "admin"));
+	}
+	
+	static void test_getUserRole() {
 		UserDAOImpl userDAO = (UserDAOImpl) ctx.getBean("userDAO");
 		User user = userDAO.getUserDetails("jag123", "admin");
 		Role role = userDAO.getUserRole(user.getRole());
-		System.out.println(userDAO.validateUser("jag123", "admin"));
-		System.out.println(user.getFirstName());
-		System.out.println(role.getAccessUM());
+		System.out.println(role.getRole());
+	}
+	
+	static void test_getAllUsersByUsername() {
+		UserDAOImpl userDAO = (UserDAOImpl) ctx.getBean("userDAO");
+		List<User> testList = userDAO.getAllUsersByUsername("");
+		for(User us : testList ) {
+			System.out.println(us.getFirstName() + " " + us.getLastName());
+		}
 	}
 
 }
