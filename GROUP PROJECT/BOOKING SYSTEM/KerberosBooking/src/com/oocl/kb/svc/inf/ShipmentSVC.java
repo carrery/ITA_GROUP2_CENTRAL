fc6b5e8a16443386029f238da4ef839c7d447ecb
Gayle Bituin <bituiga@oocl.com>
2017-09-10 12:49:59 +0800

    updateShipment GSON

--- GROUP PROJECT/BOOKING SYSTEM/KerberosBooking/src/com/oocl/kb/svc/inf/ShipmentSVC.java
+++ GROUP PROJECT/BOOKING SYSTEM/KerberosBooking/src/com/oocl/kb/svc/inf/ShipmentSVC.java
@@ -23,6 +23,5 @@ public interface ShipmentSVC {
 
 	public List<Shipment> getAllShipments(String username);
 
-	public ServiceResponse updateShipment(String shpNum, String fromCity, String toCity, Date fromDate, Date toDate,
-			String shipper, String consignee, int approveDoc, int validWeight, int goodCustomer, String shipmentStatus);
+	public ServiceResponse updateShipment(String shpNum, String fromCity);
 }
